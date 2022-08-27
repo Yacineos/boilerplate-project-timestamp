34ea22e8443e3227d33556747c8458f5f4c99957
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-08-28 01:56:53 +0200

    Debugging Left Co-authored-by: Hamza <Hamziss@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,31 +2,52 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+function dateIsValid(date) {
+  return date instanceof Date && !isNaN(date);
+}
 
-// your first API endpoint... 
+app.get("/api/:dateOrTimeStamp", (req, res) => {
+  const { dateOrTimeStamp } = req.params;
+  if ((dateOrTimeStamp == "")) {
+    var resToSend = {unix: Date.now(), utc: Date.getUTCDate()}
+    res.json(resToSend);
+  } else {
+    if (dateIsValid(dateOrTimeStamp)) {
+      var resToSend = {unix: dateOrTimeStamp.getTime(), utc: dateOrTimeStamp.toUTCString()};
+      res.json(resToSend);
+    } else {
+      if (dateOrTimeStamp > 0 && dateOrTimeStamp < Date.now()) {
+        var resToSend = {unix: dateOrTimeStamp, utc: dateOrTimeStamp.toUTCString()};
+        res.json(resToSend);
+      } else {
+        res.status(400).json({ error: "Invalid Date" });
+      }
+    }
+  }
+});
+
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
---
+++ js.js
@@ -0,0 +1,8 @@
+//console.log(Date.now());
+// const date =new Date("2021-11-06").getTime()
+// console.log(date);
+// const date = new Date("2021-11-06").toUTCString()
+// console.log(date)
+
+const date = new Date(2148639276).toUTCString()
+console.log(date)
