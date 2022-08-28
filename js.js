8488098cbbf09941055685c34b3134417a9a77f1
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-08-28 02:23:47 +0200

    DONE

--- js.js
+++
@@ -1,8 +0,0 @@
-//console.log(Date.now());
-// const date =new Date("2021-11-06").getTime()
-// console.log(date);
-// const date = new Date("2021-11-06").toUTCString()
-// console.log(date)
-
-const date = new Date(1451001600000).toUTCString()
-console.log(date)
